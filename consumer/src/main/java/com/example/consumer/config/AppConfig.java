894e2bcab13c9ec9a3b075ddacafd3f68674883b
sudarsana kasireddy <sudaredd@gmail.com>
2022-11-02 04:53:38 -0400

    update broker property

--- consumer/src/main/java/com/example/consumer/config/AppConfig.java
+++ consumer/src/main/java/com/example/consumer/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.example.consumer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -10,6 +11,7 @@ import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.TopicPartitionOffset;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -21,10 +23,11 @@ public class AppConfig {
     @Bean
     ApplicationRunner setPartiton(KafkaListenerEndpointRegistry registry,
                                   ConcurrentKafkaListenerContainerFactory<String, String> factory,
-                                  Environment environment) {
+                                  Environment environment, KafkaProperties kafkaProperties) {
         return args -> {
+//            kafkaProperties.setBootstrapServers(List.of("localhost:9090"));
             factory.getConsumerFactory().updateConfigs(Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "http://localhost:9092"));
+                "localhost:9092"));
             factory.setBatchListener(true);
             int partition = Integer.parseInt(Objects.requireNonNull(environment.getProperty("PARTITION")));
             String topic = environment.getProperty("TOPIC");
