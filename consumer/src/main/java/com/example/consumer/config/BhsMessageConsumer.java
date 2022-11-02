2744d800d9f3d1476e6514a7ef58bb71cad3e335
sudarsana kasireddy <sudaredd@gmail.com>
2022-11-02 02:58:18 -0400

    Add second listener without assigning partition

--- consumer/src/main/java/com/example/consumer/config/BhsMessageConsumer.java
+++ consumer/src/main/java/com/example/consumer/config/BhsMessageConsumer.java
@@ -18,4 +18,12 @@ public class BhsMessageConsumer {
         log.info(payload);
         log.info("headers partition {}, topic {}, timestamp {}", partition, topic, ts);
     }
+
+    @KafkaListener(topics = "${TOPIC}", groupId = "byConsumer")
+    void listenMsg(@Payload String payload,  @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+                @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                @Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts) {
+        log.info(payload);
+        log.info("headers partition {}, topic {}, timestamp {}", partition, topic, ts);
+    }
 }
